0bddfe5bf8d5dd7879d3d762c84522962b6e41e1
Sergey Pepyakin <s.pepyakin@gmail.com>
2017-08-01 10:19:40 +0300

    Try fix travis

--- binaryen-sys/build.rs
+++ binaryen-sys/build.rs
@@ -19,8 +19,9 @@ fn gen_bindings() {
 
 fn main() {
     if !Path::new("binaryen/.git").exists() {
-        let _ = Command::new("git").args(&["submodule", "update", "--init"])
-                                   .status();
+        let _ = Command::new("git")
+            .args(&["submodule", "update", "--init"])
+            .status();
     }
 
     gen_bindings();
@@ -38,5 +39,26 @@ fn main() {
     println!("cargo:rustc-link-lib=static=support");
     println!("cargo:rustc-link-lib=static=wasm");
     println!("cargo:rustc-link-lib=static=emscripten-optimizer");
-    println!("cargo:rustc-flags=-l dylib=c++");
+
+    // We need to link against C++ std lib
+    if let Some(cpp_stdlib) = get_cpp_stdlib() {
+        println!("cargo:rustc-link-lib={}", cpp_stdlib);
+    }
+}
+
+// See https://github.com/alexcrichton/gcc-rs/blob/88ac58e25/src/lib.rs#L1197
+fn get_cpp_stdlib() -> Option<String> {
+    env::var("TARGET").ok().and_then(|target| {
+        if target.contains("msvc") {
+            None
+        } else if target.contains("darwin") {
+            Some("c++".to_string())
+        } else if target.contains("freebsd") {
+            Some("c++".to_string())
+        } else if target.contains("musl") {
+            Some("static=stdc++".to_string())
+        } else {
+            Some("stdc++".to_string())
+        }
+    })
 }
