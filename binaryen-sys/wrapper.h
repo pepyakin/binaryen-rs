924057a2406f874f5ec2d0880c987889ece4cefb
Sergey Pepyakin <s.pepyakin@gmail.com>
2017-08-23 17:32:12 +0300

    Add Add/Set/Get Global functions

--- binaryen-sys/wrapper.h
+++ binaryen-sys/wrapper.h
@@ -1,1 +1,6 @@
 #include "binaryen/src/binaryen-c.h"
+#include <stdbool.h>
+
+BinaryenImportRef BinaryenAddGlobal(BinaryenModuleRef module, const char* name, BinaryenType type, int mutable_, BinaryenExpressionRef init);
+BinaryenExpressionRef BinaryenGetGlobal(BinaryenModuleRef module, const char *name, BinaryenType type);
+BinaryenExpressionRef BinaryenSetGlobal(BinaryenModuleRef module, const char *name, BinaryenExpressionRef value);
