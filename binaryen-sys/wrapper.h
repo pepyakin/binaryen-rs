3834faa438c1733113dd2958fe4bd58488181e58
Alex Beregszaszi <alex@rtfs.hu>
2019-01-02 23:39:07 +0000

    Include wrapper.h in Shim.cpp to enforce signature checks

--- binaryen-sys/wrapper.h
+++ binaryen-sys/wrapper.h
@@ -1,7 +1,15 @@
 #include "binaryen/src/binaryen-c.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 BinaryenModuleRef BinaryenModuleSafeRead(const char* input, size_t inputSize);
 
 void BinaryenShimDisposeBinaryenModuleAllocateAndWriteResult(
     BinaryenModuleAllocateAndWriteResult result
 );
+
+#ifdef __cplusplus
+}
+#endif
